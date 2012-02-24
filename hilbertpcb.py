c9c2f095c38ef9bf13e268cb748269cb56137600
Nirav Patel <nrp@eclecti.cc>
2012-02-24 00:49:10 -0800

    functioning board generation

--- hilbertpcb.py
+++ hilbertpcb.py
@@ -37,7 +37,8 @@ class HeatedTraceCalculator(object):
 class HilbertTrace(Hilbert, object):
     def __init__(self, width, order):
         Hilbert.__init__(self, 2, order)
-        self.width = width*1000.0
+        # KiCad uses 0.0001 inch unit measurements internally
+        self.width = width*10000.0
         
     def euclidean(self):
         """
@@ -69,94 +70,123 @@ class HilbertTrace(Hilbert, object):
 
    
 class PCBWriter(object):
-    def __init__(self, filename):
+    def __init__(self, filename, size, width, segments):
         self.filename = filename
+        self.segments = segments
+        self.size = size
+        self.width = width
+        self.offset = 10000
+        print self.header()
+        print self.edge([self.offset, self.offset], [self.offset+self.size[0], self.offset])
+        print self.edge([self.offset, self.offset], [self.offset, self.offset+self.size[1]])
+        print self.edge([self.offset+self.size[0], self.offset], [self.offset+self.size[0], self.offset+self.size[1]])
+        print self.edge([self.offset, self.offset+self.size[1]], [self.offset+self.size[0], self.offset+self.size[1]])
+        print "$TRACK"
+        for seg in self.segments:
+            print self.trace([seg[0][0]+self.offset, seg[0][1]+self.offset],\
+                       [seg[1][0]+self.offset, seg[1][1]+self.offset],\
+                       self.width)
+        print "$EndTRACK"
+        print self.footer()
         
     def header(self):
-        return dedent("""PCBNEW-BOARD Version 1 date
-            
-            # Hacked together using nrp's Hilbert curve PCB generator
-            
-            $GENERAL
-            encoding utf-8
-            LayerCount 2
-            Ly 1FFF8001
-            EnabledLayers 1FFF8001
-            Links 0
-            NoConn 0
-            Di 0 0 0 0
-            Ndraw 0
-            Ntrack 0
-            Nzone 0
-            BoardThickness 630
-            Nmodule 0
-            Nnets 1
-            $EndGENERAL
+        return """PCBNEW-BOARD Version 1 date
 
-            $SHEETDESCR
-            Sheet A3 16535 11700
-            Title ""
-            Date "24 feb 2012"
-            Rev ""
-            Comp ""
-            Comment1 ""
-            Comment2 ""
-            Comment3 ""
-            Comment4 ""
-            $EndSHEETDESCR
+# Hacked together using nrp's Hilbert curve PCB generator
 
-            $SETUP
-            InternalUnit 0.000100 INCH
-            Layers 2
-            Layer[0] Back signal
-            Layer[15] Front signal
-            TrackWidth 100
-            TrackClearence 100
-            ZoneClearence 200
-            TrackMinWidth 100
-            DrawSegmWidth 150
-            EdgeSegmWidth 150
-            ViaSize 350
-            ViaDrill 250
-            ViaMinSize 350
-            ViaMinDrill 200
-            MicroViaSize 200
-            MicroViaDrill 50
-            MicroViasAllowed 0
-            MicroViaMinSize 200
-            MicroViaMinDrill 50
-            TextPcbWidth 120
-            TextPcbSize 600 800
-            EdgeModWidth 150
-            TextModSize 600 600
-            TextModWidth 120
-            PadSize 600 600
-            PadDrill 320
-            Pad2MaskClearance 100
-            AuxiliaryAxisOrg 0 0
-            PcbPlotParams (pcbplotparams (layerselection 3178497) (usegerberextensions true) (excludeedgelayer true) (linewidth 60) (plotframeref false) (viasonmask false) (mode 1) (useauxorigin false) (hpglpennumber 1) (hpglpenspeed 20) (hpglpendiameter 15) (hpglpenoverlay 2) (pscolor true) (psnegative false) (psa4output false) (plotreference true) (plotvalue true) (plotothertext true) (plotinvisibletext false) (padsonsilk false) (subtractmaskfromsilk false) (outputformat 1) (mirror false) (drillshape 1) (scaleselection 1) (outputdirectory ""))
-            $EndSETUP
+$GENERAL
+encoding utf-8
+LayerCount 2
+Ly 1FFF8001
+EnabledLayers 1FFF8001
+Links 0
+NoConn 0
+Di %(xi)s %(yi)s %(xr)s %(yb)s
+Ndraw 4
+Ntrack %(track)s
+Nzone 0
+BoardThickness 630
+Nmodule 0
+Nnets 1
+$EndGENERAL
 
-            $EQUIPOT
-            Na 0 ""
-            St ~
-            $EndEQUIPOT
-            $NCLASS
-            Name "Default"
-            Desc "This is the default net class."
-            Clearance 100
-            TrackWidth 100
-            ViaDia 350
-            ViaDrill 250
-            uViaDia 200
-            uViaDrill 50
-            AddNet ""
-            $EndNCLASS""")
+$SHEETDESCR
+Sheet A3 16535 11700
+Title ""
+Date "24 feb 2012"
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndSHEETDESCR
+
+$SETUP
+InternalUnit 0.000100 INCH
+Layers 2
+Layer[0] Back signal
+Layer[15] Front signal
+TrackWidth 100
+TrackClearence 100
+ZoneClearence 200
+TrackMinWidth 100
+DrawSegmWidth 150
+EdgeSegmWidth 150
+ViaSize 350
+ViaDrill 250
+ViaMinSize 350
+ViaMinDrill 200
+MicroViaSize 200
+MicroViaDrill 50
+MicroViasAllowed 0
+MicroViaMinSize 200
+MicroViaMinDrill 50
+TextPcbWidth 120
+TextPcbSize 600 800
+EdgeModWidth 150
+TextModSize 600 600
+TextModWidth 120
+PadSize 600 600
+PadDrill 320
+Pad2MaskClearance 100
+AuxiliaryAxisOrg 0 0
+PcbPlotParams (pcbplotparams (layerselection 3178497) (usegerberextensions true) (excludeedgelayer true) (linewidth 60) (plotframeref false) (viasonmask false) (mode 1) (useauxorigin false) (hpglpennumber 1) (hpglpenspeed 20) (hpglpendiameter 15) (hpglpenoverlay 2) (pscolor true) (psnegative false) (psa4output false) (plotreference true) (plotvalue true) (plotothertext true) (plotinvisibletext false) (padsonsilk false) (subtractmaskfromsilk false) (outputformat 1) (mirror false) (drillshape 1) (scaleselection 1) (outputdirectory ""))
+$EndSETUP
+
+$EQUIPOT
+Na 0 ""
+St ~
+$EndEQUIPOT
+$NCLASS
+Name "Default"
+Desc "This is the default net class."
+Clearance 100
+TrackWidth 100
+ViaDia 350
+ViaDrill 250
+uViaDia 200
+uViaDrill 50
+AddNet ""
+$EndNCLASS""" % {"xi" : self.offset-101, "yi" : self.offset-101,\
+                 "xr" : self.offset+self.size[0]+101, "yb" : self.offset+self.size[1]+101,\
+                 "track" : len(self.segments)}
             
     def footer(self):
-        return dedent("""$ZONE
-            $EndZONE
-            $EndBOARD""")
+        return """$ZONE
+$EndZONE
+$EndBOARD"""
+
+    def edge(self, start, end):
+        return """$EndDRAWSEGMENT
+$DRAWSEGMENT
+Po 0 %(xi)s %(yi)s %(xr)s %(yb)s 100
+De 28 0 900 0 0
+$EndDRAWSEGMENT""" % {"xi" : start[0], "yi" : start[1], "xr" : end[0], "yb" : end[1]}
+
+    def trace(self, start, end, width):
+        return """Po 0 %(xi)s %(yi)s %(xr)s %(yb)s %(w)s -1
+De 15 0 0 0 0""" % {"xi" : start[0], "yi" : start[1], "xr" : end[0], "yb" : end[1], "w" : width}
 
 
 class PCBGenerator(object):
@@ -173,8 +203,10 @@ class PCBGenerator(object):
     def blah(self, order):
         h = HilbertTrace(self.pcb_width, order)
         count = len(h)
+        ret = []
         for i in range(0, count-1):
-            print h.segment(i)
+            ret.append(h.segment(i))
+        return ret
         
     def length(self):
         min_width = self.trace.min_width()
@@ -207,5 +239,5 @@ class PCBGenerator(object):
 
 if __name__ == '__main__':
     generator = PCBGenerator(12, 100, (5, 5))
-    print generator.electrical_description()
-    print generator.length()
+    lmin, lmax = generator.length()
+    PCBWriter("blah", [50000, 50000], int(lmax[2]), generator.blah(6))
